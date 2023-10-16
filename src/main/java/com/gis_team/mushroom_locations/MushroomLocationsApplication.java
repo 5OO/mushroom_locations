ec92b8208bc58664c1cafa6346db753c136f447f
5OO <git.hub@myrk.eu>
2023-10-16 11:37:48 +0300

    Initial commit - project setup

---
+++ src/main/java/com/gis_team/mushroom_locations/MushroomLocationsApplication.java
@@ -0,0 +1,13 @@
+package com.gis_team.mushroom_locations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomLocationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MushroomLocationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gis_team/mushroom_locations/MushroomLocationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis_team.mushroom_locations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomLocationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
